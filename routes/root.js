8f7262a779b25b2ef403fd26b662d1192de2230f
Sebastien Dunne <dunn0203@algonquinlive.com>
2025-11-30 11:54:29 -0500

    added workflow

--- routes/root.js
+++ routes/root.js
@@ -1,18 +1,19 @@
-'use strict'
+"use strict";
 
 module.exports = async function (fastify, opts) {
-  fastify.post('/', async function (request, reply) {
-    const msg = request.body
-    fastify.sendMessage(Buffer.from(JSON.stringify(msg)))
-    reply.code(201)
-  })
+  fastify.post("/", async function (request, reply) {
+    console.log("hello");
+    const msg = request.body;
+    fastify.sendMessage(Buffer.from(JSON.stringify(msg)));
+    reply.code(201);
+  });
 
-  fastify.get('/health', async function (request, reply) {
-    const appVersion = process.env.APP_VERSION || '0.1.0'
-    return { status: 'ok', version: appVersion }
-  })
+  fastify.get("/health", async function (request, reply) {
+    const appVersion = process.env.APP_VERSION || "0.1.0";
+    return { status: "ok", version: appVersion };
+  });
 
-  fastify.get('/hugs', async function (request, reply) {
-    return { hugs: fastify.someSupport() }
-  })
-}
+  fastify.get("/hugs", async function (request, reply) {
+    return { hugs: fastify.someSupport() };
+  });
+};
